e28e5bf242621cac184fb9b1e03a13800a5bc23d
VlBl <v95506442@gmail.com>
2026-02-28 20:48:25 +0000

    adminer

---
+++ app/Models/Account.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Account extends Model
+{
+    /** @use HasFactory<\Database\Factories\AccountFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'card_num',
+        'balance',
+        'currency',
+        'type',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2026_02_28_203912_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id')->constrained('users');
+
+            $table->tinyText('card_num');
+            $table->integer('balance');
+            $table->tinyText('currency');
+            $table->enum('type', ['debit', 'credit']);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
